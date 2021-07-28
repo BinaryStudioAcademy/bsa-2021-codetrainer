fbe6dc5084faffd101b80dc24844260097e06dd1
dammmula <eliza04102001@gmail.com>
2021-07-28 17:18:38 +0300

    Add profileBio component

--- client/src/containers/Profile/index.tsx
+++ client/src/containers/Profile/index.tsx
@@ -4,6 +4,7 @@ import ProfileRouter from './ProfileRouter'
 import Stats from './Stats'
 import '../../styles/index.scss'
 import '../../styles/profile.scss'
+import { ProfileBio } from './ProfileBio';
 
 
 interface ProfileProps {
@@ -14,6 +15,19 @@ interface ProfileProps {
     }
 }
 
+const mockProfileBioProps = {
+    img: 'https://hyperhost.ua/info/storage/avatars/user-ava.png',
+    name: 'name',
+    nickname: 'nickname',
+    clan: 'clan',
+    memberSince: '14 jul',
+    lastSeen: '13 sep',
+    gitHub: 'github link',
+    following: 0,
+    followers: 0,
+    community: 3
+}
+
 export const Profile: React.FC<ProfileProps> = (props) => {
     //const userName: string= props.match.params.name;
     const match = useRouteMatch();
@@ -22,8 +36,7 @@ export const Profile: React.FC<ProfileProps> = (props) => {
         <>
         <div className="profile">
             <div className="profile-bio">
-               Top Block
-            {/* top block here */}
+               <ProfileBio {...mockProfileBioProps}/>
             </div>
             <div className="profile-info">
                 <ProfileRouter/>
