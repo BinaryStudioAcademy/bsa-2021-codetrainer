c9ee9269248d1c14184157c22417cf00d658e92a
Paul Vass <maincrafter1307@gmail.com>
2021-07-28 17:18:37 +0300

    hot fixes

--- client/src/containers/Profile/StatsComponents/Points.tsx
+++ client/src/containers/Profile/StatsComponents/Points.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import pointsIcon from '../../../assets/points.svg'
-
+import '../../../styles/stats.scss'
 
 const Points: React.FC = () => {
     return (
         <div>
             <img src={pointsIcon} id="pointsIcon" />
-            <label htmlFor="pointsIcon">Points</label>
+            <label htmlFor="pointsIcon" className="iconLabel">Points</label>
             <p>
-                <span className="field-name">Rank</span>
+                <span className="field-name">Rank:</span>
                 <span className="field-value">4</span>
             </p>
             <p>
-                <span className="field-name">Honor</span>
+                <span className="field-name">Honor:</span>
                 <span className="field-value">455</span>
             </p>
             <p>
-                <span className="field-name">Total Completed Challenge</span>
+                <span className="field-name">Total Completed Challenge:</span>
                 <span className="field-value">17</span>
             </p>
         </div>
