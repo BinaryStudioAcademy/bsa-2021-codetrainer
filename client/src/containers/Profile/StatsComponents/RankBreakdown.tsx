8a4ac749ca38fc3b36ce5949e0371a81480b524c
Paul Vass <maincrafter1307@gmail.com>
2021-07-28 23:58:36 +0300

    added info of progress-circle-bar

--- client/src/containers/Profile/StatsComponents/RankBreakdown.tsx
+++ client/src/containers/Profile/StatsComponents/RankBreakdown.tsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import rankBreakdownIcon from '../../../assets/rankBreakdown.svg'
 import '../../../styles/rankBreakdown.scss'
-import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import { buildStyles, CircularProgressbarWithChildren } from 'react-circular-progressbar';
 
 const RankBreakdown: React.FC = () => {
     const percentage = 25;
 
     return (
-        <div>
-            <div className='header rank-breakdown'>
+        <div className="rank-breakdown">
+            <div className='header'>
                 <img src={rankBreakdownIcon} id="rankBreakdownIcon"/>
                 <label htmlFor="rankBreakdownIcon" className="iconLabel">Rank Breakdown</label>
-                <CircularProgressbar
+            </div>
+            <div style={{width: "150px"}}> 
+            <CircularProgressbarWithChildren
                     className='circular-progressbar'
                     value={percentage}
-                    text='Next Rank'
                     strokeWidth={5}
                     styles={buildStyles(circularProgressBarStyles)}
-                />
+                >
+
+                <strong className="circularProgress" style={{fontSize: "16px",paddingBottom: "50px"}}>Next Rank</strong>
+                </CircularProgressbarWithChildren>
+            </div>
+            <div className="progress-circle-info">
+                <strong>Overall: </strong><span>8</span>ran/<span>10</span>%
             </div>
         </div>
         )
@@ -25,7 +32,7 @@ const RankBreakdown: React.FC = () => {
 export default RankBreakdown;
 
 const circularProgressBarStyles = {
-    // strokeLinecap: 'butt',
+     strokeLinecap: 'butt',
     textSize: '14px',
     // pathTransitionDuration: 0.5,
     pathColor: `#ec4179`,
