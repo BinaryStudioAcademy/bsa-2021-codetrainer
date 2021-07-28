d711243050a6b292320c4eabacddf33d688fab87
svitlanaShymko <svitlana.shymko@cg24.com>
2021-07-28 17:21:47 +0400

    add some styling

--- client/src/components/FormInput/index.tsx
+++ client/src/components/FormInput/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Field } from 'formik';
+import styles from './FormInput.module.scss';
 
 type Props = {
     id: string;
     name: string;
     label: string;
     type: string;
+    placeholder: string;
 }
 
-const FormInput: React.FC<Props> = ({id, name, label, type})=>{
+const FormInput: React.FC<Props> = ({id, name, label, type, placeholder})=>{
     return(
         <div>
-        <label htmlFor={id}>{label}</label>
-        <Field id={id} name={name} type={type} />
+        <label htmlFor={id} className={styles.label} >{label}</label>
+        <Field id={id} name={name} type={type} placeholder={placeholder} className={styles.inputField}/>
         </div>
     );
 }
--- client/src/components/SignUp/index.tsx
+++ client/src/components/SignUp/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from 'components/Layout';
 import FormInput from 'components/FormInput';
 import { Formik, Form } from 'formik';
+import styles from './SignUp.module.css'
 
 const SignUp: React.FC = () =>{
     return (
@@ -22,33 +23,38 @@ const SignUp: React.FC = () =>{
                 id="firstName"
                 name="firstName"
                 label="First Name"
+                placeholder="Enter your first name"
                 type="text"
                 />
                 <FormInput
                 id="lastName"
                 name="lastName"
                 label="Last Name"
+                placeholder="Enter your last name"
                 type="text"
                 />
                 <FormInput
                 id="email"
                 name="email"
                 label="Email"
+                placeholder="Enter your email"
                 type="email"
                 />
                 <FormInput
                 id="password"
                 name="password"
                 label="Password"
+                placeholder="********"
                 type="password"
                 />
                 <FormInput
                 id="confirmPassword"
                 name="confirmPassword"
                 label="Confirm Password"
+                placeholder="********"
                 type="password"
                 />
-                <button type="submit">Sign Up</button>
+                <button type="submit" className={styles.submitBtn}>Sign Up</button>
             </Form>
             </Formik>
         </Layout>
