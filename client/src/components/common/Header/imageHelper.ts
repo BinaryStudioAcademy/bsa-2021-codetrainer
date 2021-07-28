ccde6f64cfa11ee96b754b0df23906335873856e
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2021-07-29 02:52:20 +0300

    added imageHelper

---
+++ client/src/components/common/Header/imageHelper.ts
@@ -0,0 +1,13 @@
+import bell from 'assets/header/bell.svg';
+import menuProfile from 'assets/header/menu/avatar.svg';
+import menuSettings from 'assets/header/menu/settings.svg';
+import menuChallenge from 'assets/header/menu/goal.svg';
+import menuSignout from 'assets/header/menu/logout.svg';
+
+export {
+	bell, 
+	menuProfile, 
+	menuSettings, 
+	menuChallenge, 
+	menuSignout
+}
