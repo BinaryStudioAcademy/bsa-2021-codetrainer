dcd6d7a38c0ac5cde11f57515126167bd8dca81a
INTJT <intjtlmao@gmail.com>
2021-07-28 16:19:47 +0300

    add cover layout

---
+++ client/src/components/CoverLayout/index.tsx
@@ -0,0 +1,22 @@
+import React, { PropsWithChildren, FC } from 'react';
+import background from 'assets/cover-background.svg';
+import logo from 'assets/logo.svg';
+import styles from './cover.module.scss';
+
+interface CoverProps {};
+
+const CoverLayout: FC<PropsWithChildren<CoverProps>> = ({ children }) => {
+	return (
+		<div className={styles.cover}>
+			<div className={styles.image}>
+				<img className={styles.background} src={background} alt=""/>
+				<img className={styles.logo} src={logo} alt="codetrainer"/>
+			</div>
+			<div>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export default CoverLayout;
