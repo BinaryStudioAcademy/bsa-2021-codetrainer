f94ef168a30523515fb0bd04b72bd74d279ec6b5
Vlad Zusko <vladzvu11@gmail.com>
2021-07-28 19:53:45 +0300

    Added component styles

--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import history from 'helpers/history.helper';
 import store from 'redux/store';
 import Routing from 'containers/Routing';
+import '../../styles/index.scss';
 
 const App: React.FC = () => {
 	return (
