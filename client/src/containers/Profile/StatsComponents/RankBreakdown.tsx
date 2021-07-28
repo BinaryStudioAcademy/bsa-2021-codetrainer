ab5a335fe4b1258e4f4517d1083eb340f39d5cd3
dammmula <eliza04102001@gmail.com>
2021-07-28 21:59:39 +0300

    Make circular progressbar

--- client/src/containers/Profile/StatsComponents/RankBreakdown.tsx
+++ client/src/containers/Profile/StatsComponents/RankBreakdown.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import rankBreakdownIcon from '../../../assets/rankBreakdown.svg'
-import '../../../styles/stats.scss'
+import '../../../styles/rankBreakdown.scss'
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 
 const RankBreakdown: React.FC = () => {
+    const percentage = 25;
+
     return (
         <div>
-            <div className='header'>
+            <div className='header rank-breakdown'>
                 <img src={rankBreakdownIcon} id="rankBreakdownIcon"/>
                 <label htmlFor="rankBreakdownIcon" className="iconLabel">Rank Breakdown</label>
+                <CircularProgressbar
+                    className='circular-progressbar'
+                    value={percentage}
+                    text='Next Rank'
+                    strokeWidth={5}
+                    styles={buildStyles(circularProgressBarStyles)}
+                />
             </div>
         </div>
         )
 }
-export default RankBreakdown
+export default RankBreakdown;
+
+const circularProgressBarStyles = {
+    // strokeLinecap: 'butt',
+    textSize: '14px',
+    // pathTransitionDuration: 0.5,
+    pathColor: `#ec4179`,
+    // textColor: '#f88',
+    trailColor: '#F0F3F9',
+    backgroundColor: '#F0F3F9',
+}
