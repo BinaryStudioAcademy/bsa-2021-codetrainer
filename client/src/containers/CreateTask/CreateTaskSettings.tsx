978be987ca0d38286c370589cb6f4bed129ce40d
Paul Vass <maincrafter1307@gmail.com>
2021-07-29 14:38:00 +0300

    current commit

--- client/src/containers/CreateTask/CreateTaskSettings.tsx
+++ client/src/containers/CreateTask/CreateTaskSettings.tsx
@@ -1,22 +1,27 @@
 import React, { FormEvent } from 'react';
 import {Classes, H2,Label, RadioGroup, Radio} from '@blueprintjs/core'
 import { useState } from 'react';
+import './create-task-settings.scss'
 interface Props {}
 
 const CreateTaskSettings: React.FC<Props> = () => {
     const [radioSelected, setRadioSelected] = useState("one")
     return (
-    <div>
-        <H2 className="heading">Hello</H2>
+    <div className="create-task-settings">
+        <H2 className="heading">Create a New Task</H2>
         <form>
-            <Label htmlFor="task-name">Name</Label>
-            <input className={Classes.INPUT} id="task-name" placeholder="Enter task name"/>
+            <Label htmlFor="task-name" >Name</Label>
+            <input className={Classes.INPUT} id="task-name" placeholder="Enter Task Name"/>
 
             <RadioGroup inline={true} 
+            className="discipline-label"
             label="Discipline"
             onChange={(value:FormEvent<HTMLInputElement>)=>setRadioSelected(value.currentTarget.value)}
             selectedValue={radioSelected}>
+                <div>
+                <p>hello!</p>
                 <Radio label="Fundamentals" value="Fundamentals"/>
+                </div>
                 <Radio label="Rank Up" value="Rank Up"/>
                 <Radio label="Practice and Repeat" value="Practice and Repeat"/>
                 <Radio label="Beta" value="Beta"/>
