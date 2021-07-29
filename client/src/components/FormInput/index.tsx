dd6a81e790b2beecf2e0e5d0a3867080547bef3b
svitlanaShymko <svitlana.shymko@cg24.com>
2021-07-29 10:03:11 +0400

    add password visibility button

--- client/src/components/FormInput/index.tsx
+++ client/src/components/FormInput/index.tsx
@@ -1,6 +1,7 @@
+import React, { useState } from 'react';
 import { FieldProps, getIn } from 'formik';
-import React from 'react';
 import styles from './FormInput.module.scss';
+import { Icon } from "@blueprintjs/core";
 
 type Props = FieldProps & {
     id: string;
@@ -13,13 +14,20 @@ type Props = FieldProps & {
 const FormInput: React.FC<Props> = ({
         id,
         label,
-        type,
+        type: initialType,
         placeholder,
         field: { name, value, onChange},
         form: {errors, touched}
     })=>{
         const error = getIn(errors, name);
         const isTouched = getIn(touched, name);
+        const [type, setType] = useState(initialType);
+        const isPasswordField = initialType === 'password';
+
+        const togglePasswordVisibility = () =>{
+            const newType = type==='password' ? 'text' : 'password';
+            setType(newType);
+        }
 
     return(
         <div className={styles.inputWrapper}>
@@ -31,8 +39,9 @@ const FormInput: React.FC<Props> = ({
                 value={value}
                 type={type}
                 placeholder={placeholder}
-                className={styles.inputField}
+                className={isPasswordField ? styles.passwordField : styles.inputField}
             />
+            {isPasswordField  && <Icon onClick={togglePasswordVisibility} icon="eye-off" className={styles.visibilityBtn}/>}
             {isTouched && error && <div className={styles.error}>{error}</div>}
         </div>
     );
