7b53bb4f4c107c73669731035298cde8b8721d3b
svitlanaShymko <svitlana.shymko@cg24.com>
2021-07-29 18:09:29 +0400

    lint-fix

--- client/src/components/CoverLayout/index.tsx
+++ client/src/components/CoverLayout/index.tsx
@@ -3,18 +3,16 @@ import background from 'assets/cover-background.svg';
 import logo from 'assets/logo.svg';
 import styles from './cover.module.scss';
 
-interface CoverProps {};
+interface CoverProps {}
 
 const CoverLayout: FC<PropsWithChildren<CoverProps>> = ({ children }) => {
 	return (
 		<div className={styles.cover}>
 			<div className={styles.image}>
-				<img className={styles.background} src={background} alt=""/>
-				<img className={styles.logo} src={logo} alt="codetrainer"/>
+				<img className={styles.background} src={background} alt="" />
+				<img className={styles.logo} src={logo} alt="codetrainer" />
 			</div>
-			<main className={styles.main}>
-				{children}
-			</main>
+			<main className={styles.main}>{children}</main>
 		</div>
 	);
 };
--- client/src/components/FormInput/index.tsx
+++ client/src/components/FormInput/index.tsx
@@ -1,50 +1,54 @@
 import React, { useState } from 'react';
 import { FieldProps, getIn } from 'formik';
 import styles from './FormInput.module.scss';
-import { Icon } from "@blueprintjs/core";
+import { Icon } from '@blueprintjs/core';
 
 type Props = FieldProps & {
-    id: string;
-    name: string;
-    label: string;
-    type: string;
-    placeholder: string;
-}
+	id: string;
+	name: string;
+	label: string;
+	type: string;
+	placeholder: string;
+};
 
 const FormInput: React.FC<Props> = ({
-        id,
-        label,
-        type: initialType,
-        placeholder,
-        field: { name, value, onChange},
-        form: {errors, touched}
-    })=>{
-        const error = getIn(errors, name);
-        const isTouched = getIn(touched, name);
-        const [type, setType] = useState(initialType);
-        const isPasswordField = initialType === 'password';
+	id,
+	label,
+	type: initialType,
+	placeholder,
+	field: { name, value, onChange },
+	form: { errors, touched },
+}) => {
+	const error = getIn(errors, name);
+	const isTouched = getIn(touched, name);
+	const [type, setType] = useState(initialType);
+	const isPasswordField = initialType === 'password';
 
-        const togglePasswordVisibility = () =>{
-            const newType = type==='password' ? 'text' : 'password';
-            setType(newType);
-        }
+	const togglePasswordVisibility = () => {
+		const newType = type === 'password' ? 'text' : 'password';
+		setType(newType);
+	};
 
-    return(
-        <div className={styles.inputWrapper}>
-            <label htmlFor={id} className={styles.label}>{label}</label>
-            <input
-                id={id}
-                name={name}
-                onChange={onChange}
-                value={value}
-                type={type}
-                placeholder={placeholder}
-                className={isPasswordField ? styles.passwordField : styles.inputField}
-            />
-            {isPasswordField  && <Icon onClick={togglePasswordVisibility} icon="eye-off" className={styles.visibilityBtn}/>}
-            {isTouched && error && <div className={styles.error}>{error}</div>}
-        </div>
-    );
-}
+	return (
+		<div className={styles.inputWrapper}>
+			<label htmlFor={id} className={styles.label}>
+				{label}
+			</label>
+			<input
+				id={id}
+				name={name}
+				onChange={onChange}
+				value={value}
+				type={type}
+				placeholder={placeholder}
+				className={isPasswordField ? styles.passwordField : styles.inputField}
+			/>
+			{isPasswordField && (
+				<Icon onClick={togglePasswordVisibility} icon="eye-off" className={styles.visibilityBtn} />
+			)}
+			{isTouched && error && <div className={styles.error}>{error}</div>}
+		</div>
+	);
+};
 
 export default FormInput;
