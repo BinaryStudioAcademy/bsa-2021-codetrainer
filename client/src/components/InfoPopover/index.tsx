e4ca01e7425e8c6664d37b6e72419e30255b7a27
INTJT <intjtlmao@gmail.com>
2021-07-29 17:28:17 +0300

    add infoPopover

---
+++ client/src/components/InfoPopover/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Popover2 } from "@blueprintjs/popover2";
+import { FC, PropsWithChildren } from "react";
+import infoIcon from 'assets/info-popover.svg';
+import styles from './info-popover.module.scss';
+
+const InfoPopover: FC<PropsWithChildren<any>> = ({ children }) => (
+	<Popover2
+		autoFocus={false}
+		enforceFocus={false}
+		minimal={false}
+		interactionKind='hover'
+		popoverClassName={styles.infoPopover}
+		placement='auto'
+		content={children}
+	>
+		<img className={styles.icon} src={infoIcon} width={15} height={15} alt="info"/>
+	</Popover2>
+);
+
+export default InfoPopover;
