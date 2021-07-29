8b46bc4150b805b98740315895c7a59de80848a3
INTJT <intjtlmao@gmail.com>
2021-07-29 08:49:25 +0300

    add form field component

---
+++ client/src/components/FormField/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+import { Field, FieldAttributes } from 'formik';
+import { combineClasses } from 'helpers/combineClasses.helper';
+import styles from './form-field.module.scss';
+
+const FormField: FC<FieldAttributes<any>> = (props: FieldAttributes<any>) => (
+    <Field
+        {...props}
+        className={combineClasses(styles.field, props.className)}
+    />
+);
+
+export default FormField;
