a223321abbcc1d57af7a6345fbb31366fb632e93
INTJT <intjtlmao@gmail.com>
2021-07-29 08:51:25 +0300

    add button component

---
+++ client/src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import React, { ButtonHTMLAttributes, FC } from 'react';
+import { combineClasses } from 'helpers/combineClasses.helper';
+import styles from './button.module.scss';
+
+const Button: FC<ButtonHTMLAttributes<any>> = (props) => (
+    <button
+        {...props}
+        className={combineClasses(styles.button, props.className)}
+    >
+        {props.children}
+    </button>
+);
+
+export default Button;
+
+export const ButtonClasses = Object.freeze({
+    red: styles.red,
+    blue: styles.blue,
+    filled: styles.filled
+});
