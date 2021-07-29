38f0a31c9807e02e96ae27891dd96747308e47c5
trueubermensch <nikita.dnestrov@gmail.com>
2021-07-30 01:31:11 +0300

    Created Task Instructions component

--- client/src/containers/Routing/index.tsx
+++ client/src/containers/Routing/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import Example from 'containers/Example';
-import PublicRoute from 'components/PublicRoute';
+import TaskInstructions from 'containers/TaskInstructions';
+// import PublicRoute from 'components/PublicRoute';
 
 interface Props {}
 
-const Routing: React.FC<Props> = () => (
-	<Switch>
-		<PublicRoute exact restricted={false} path="/" component={Example} />
-	</Switch>
-);
+const Routing: React.FC<Props> = () => {
+	return (
+		<Switch>
+			{/* <PublicRoute exact restricted={false} path="/" component={Example} /> */}
+			<TaskInstructions
+				title="Stop gninnipS My sdroW!"
+				description="Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed."
+				demands={[
+					'Strings passed in will consist of only letters and spaces.',
+					'Spaces will be included only when more than one word is present.',
+				]}
+				examples={[
+					'spinWords("Hey fellow warriors") => "Hey wollef sroirraw" ',
+					'spinWords("This is a test") => "This is a test"',
+					'spinWords("This is another test") => "This is rehtona test"',
+				]}
+				tags={['Algorithms', 'Strings', 'Data Types', 'Formatting', 'Logic']}
+			/>
+		</Switch>
+	);
+};
 
 export default Routing;
