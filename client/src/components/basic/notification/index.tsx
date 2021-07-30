4b5ea81b40fba6406de585ee512400a5cc952241
dammmula <eliza04102001@gmail.com>
2021-07-31 00:17:28 +0300

    Add basic structure

---
+++ client/src/components/basic/notification/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+interface INotificationProps {
+}
+
+export const Notification: React.FC<INotificationProps> = (props) => {
+	return (
+		<div>
+			notification
+		</div>
+	);
+}
