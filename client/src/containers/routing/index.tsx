5fc3c2692b3a4140bd1f174b4aef0793e3dccab7
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2021-07-30 17:50:39 +0300

    refactoring

--- client/src/containers/routing/index.tsx
+++ client/src/containers/routing/index.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Example from 'containers/example';
 import { PublicRoute } from 'components';
+import Header from 'components/common/header';
+import avatar from 'assets/images/header/avatar.png';
 
 interface IRoutingProps { }
 
 const Routing: React.FC<IRoutingProps> = () => (
 	<Switch>
+		<Header
+      name = 'Rayna Herwitz'
+      rank = {8}
+      mark = {455}
+      notificationCounter = {3}
+      avatar = {avatar}
+    />
 		<PublicRoute exact restricted={false} path="/" component={Example} />
 	</Switch>
 );
