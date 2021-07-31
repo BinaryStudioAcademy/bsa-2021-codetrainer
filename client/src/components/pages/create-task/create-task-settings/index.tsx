f46d20ba5ec1b2be3f4f734510a28f026c557d0e
INTJT <intjtlmao@gmail.com>
2021-07-31 15:18:05 +0300

    refactor create-task-settings component

--- client/src/components/pages/create-task/create-task-settings/index.tsx
+++ client/src/components/pages/create-task/create-task-settings/index.tsx
@@ -1,85 +1,95 @@
-import React, { useState } from 'react';
-import { Classes, H2, H4, Label, Switch } from '@blueprintjs/core';
-import { ReactComponent as Fundamentals } from '../../../../assets/icons/books-icon.svg';
-import { ReactComponent as Rankup } from '../../../../assets/icons/rank-up-icon.svg';
-import { ReactComponent as Practice } from '../../../../assets/icons/practice-icon.svg';
-import { ReactComponent as Beta } from '../../../../assets/icons/beta-icon.svg';
-import { ReactComponent as Random } from '../../../../assets/icons/shuffle-icon.svg';
-import RadioItem from './RadioItem';
-import InfoPopover from 'components/InfoPopover';
-import './switch.scss';
+import React, { FC, SVGProps, useState } from 'react';
+import { Classes, H2, H4, Label, RadioGroup } from '@blueprintjs/core';
+import RadioItem from '../radio-item';
+import { Disciplines } from 'constants/disciplines';
+import { InfoPopover, Switch } from 'components/basic';
 import styles from './create-task-settings.module.scss';
-interface ICreateTaskSettingsProps {}
-interface IRadioItem {
-	element: React.ReactElement;
-	text: string;
-	elementClass: string;
+import { ReactComponent as FundamentalsIcon } from 'assets/icons/books-icon.svg';
+import { ReactComponent as RankUpIcon } from 'assets/icons/rank-up-icon.svg';
+import { ReactComponent as PracticeIcon } from 'assets/icons/practice-icon.svg';
+import { ReactComponent as BetaIcon } from 'assets/icons/beta-icon.svg';
+import { ReactComponent as RandomIcon } from 'assets/icons/shuffle-icon.svg';
+
+type TSvgFC = FC<SVGProps<SVGSVGElement>>;
+
+interface IDisciplineItem {
+	value: Disciplines;
+	iconFC: TSvgFC;
+	label: string;
 }
-const CreateTaskSettings: React.FC<ICreateTaskSettingsProps> = () => {
-	const [checkedState, setChecked] = useState('fundamentals');
-	let i = 0;
-	const [switchCheck, setSwitchCheck] = useState(true);
-	const RADIO_ITEMS: IRadioItem[] = [
-		{
-			element: <Fundamentals key={i++} />,
-			elementClass: 'fundamentals',
-			text: 'Fundamentals',
-		},
-		{
-			element: <Rankup key={i++} />,
-			elementClass: 'rankup',
-			text: 'Rank Up',
-		},
-		{
-			element: <Practice key={i++} />,
-			elementClass: 'practice',
-			text: 'Practice',
-		},
-		{
-			element: <Beta key={i++} />,
-			elementClass: 'beta',
-			text: 'Beta',
-		},
-		{
-			element: <Random key={i++} />,
-			elementClass: 'random',
-			text: 'Random',
-		},
-	];
+
+const DISCIPLINE_ITEMS: IDisciplineItem[] = [
+	{
+		value: Disciplines.FUNDAMENTALS,
+		iconFC: FundamentalsIcon,
+		label: 'Fundamentals',
+	},
+	{
+		value: Disciplines.RANK_UP,
+		iconFC: RankUpIcon,
+		label: 'Rank Up',
+	},
+	{
+		value: Disciplines.PRACTICE,
+		iconFC: PracticeIcon,
+		label: 'Practice',
+	},
+	{
+		value: Disciplines.BETA,
+		iconFC: BetaIcon,
+		label: 'Beta',
+	},
+	{
+		value: Disciplines.RANDOM,
+		iconFC: RandomIcon,
+		label: 'Random',
+	},
+];
+
+interface ICreateTaskSettingsProps {}
+
+const CreateTaskSettings: FC<ICreateTaskSettingsProps> = () => {
+	const [discipline, setDiscipline] = useState<Disciplines>(Disciplines.FUNDAMENTALS);
+	const [allowContributors, setAllowContributors] = useState(true);
+
 	return (
 		<div className={styles.createTaskSettings}>
 			<H2 className="heading">Create a New Task</H2>
 			<form>
 				<Label htmlFor="task-name">Name</Label>
 				<input className={Classes.INPUT} id="task-name" placeholder="Enter Task Name" />
+
 				<H4 className={styles.disciplinesHeading}>Disciplines</H4>
-				<div className={styles.radioList}>
-					{RADIO_ITEMS.map((item, index) => {
+				<RadioGroup
+					name="discipline"
+					onChange={(event) => setDiscipline(event.currentTarget.value as Disciplines)}
+					className={styles.radioList}
+					selectedValue={discipline}
+				>
+					{DISCIPLINE_ITEMS.map((item, index) => {
 						return (
 							<RadioItem
-								component={item.element}
-								checkedState={checkedState}
-								setChecked={setChecked}
-								classNameComp={item.elementClass}
-								text={item.text}
-								key={index}
+								value={item.value}
+								icon={<item.iconFC width={25} height={25} />}
+								text={item.label}
+								key={item.value}
 							/>
 						);
 					})}
-				</div>
+				</RadioGroup>
 
 				{/* 
-                        value interface: {
-                            title: string,
-                            icon: string
-                        }
-                        Select interface: {
-                            values: value[],
-                            activeValueL: value,
-                            onChange: React.Dispatch<React.SetStateAction<ISelectValue>>
-                        }
-                    */}
-				{/* <Select values={focusValues} activeValue={activeFocusValue} onChange={setActiveFocusValue} /> */}
+					value interface: {
+						title: string,
+						icon: string
+					}
+					Select interface: {
+						values: value[],
+						activeValueL: value,
+						onChange: React.Dispatch<React.SetStateAction<ISelectValue>>
+					}
+					<Select values={focusValues} activeValue={activeFocusValue} onChange={setActiveFocusValue} /> 
+				*/}
 
 				<Label htmlFor="estimated-rank">
 					Estimated Rank
@@ -90,25 +100,21 @@ const CreateTaskSettings: React.FC<ICreateTaskSettingsProps> = () => {
 				<Label htmlFor="tags">Tags</Label>
 				<input className={Classes.INPUT} id="tags" placeholder="Enter Tags (separated by comma)" />
 
-				<div>
-					<Switch
-						id="allow-contributors"
-						inline
-						checked={switchCheck}
-						onChange={() => {
-							setSwitchCheck(!switchCheck);
-						}}
-						labelElement={
-							<span>
-								Allow contributors?
-								<InfoPopover>
-									Check to allow contributors to make changes to this task while it&apos;s in beta.
-									You will be notified of any edits made by other users.
-								</InfoPopover>
-							</span>
-						}
-					/>
-				</div>
+				<Switch
+					inline
+					id="allow-contributors"
+					checked={allowContributors}
+					onChange={() => setAllowContributors(!allowContributors)}
+					labelElement={
+						<span>
+							Allow contributors?
+							<InfoPopover>
+								Check to allow contributors to make changes to this task while it&apos;s in beta. You
+								will be notified of any edits made by other users.
+							</InfoPopover>
+						</span>
+					}
+				/>
 			</form>
 		</div>
 	);
