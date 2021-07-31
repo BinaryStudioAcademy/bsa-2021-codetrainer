b58104f4ce768d451ca2abbcfc2ada5dd31bfe63
INTJT <intjtlmao@gmail.com>
2021-07-31 15:15:56 +0300

    move info-popover into basic

--- client/src/components/basic/index.ts
+++ client/src/components/basic/index.ts
@@ -1,2 +1,4 @@
 export { default as PrivateRoute } from './private-route';
 export { default as PublicRoute } from './public-route';
+export { default as InfoPopover } from './info-popover';
+export { default as Switch } from './switch';
--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,2 +1,1 @@
 export { default as Example } from './example';
-export { default as InfoPopover } from './info-popover';
