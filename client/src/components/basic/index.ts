c80c5bb0aca1df2c4aa008f66335f4fde71f7ca9
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2021-07-31 19:45:56 +0300

    added Avatar, Label to exporting

--- client/src/components/basic/index.ts
+++ client/src/components/basic/index.ts
@@ -1,2 +1,4 @@
 export { default as PrivateRoute } from './private-route';
 export { default as PublicRoute } from './public-route';
+export { default as Avatar } from './avatar';
+export { default as Label } from './label';
