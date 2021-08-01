8d33bc43ad5966972310e72f82530a611f6155b2
dammmula <eliza04102001@gmail.com>
2021-08-01 16:01:01 +0300

    Fix styles

--- client/src/components/pages/profile/profile-info/tabs/stats/index.tsx
+++ client/src/components/pages/profile/profile-info/tabs/stats/index.tsx
@@ -25,39 +25,3 @@ export const Stats: React.FC<IStatsProps> = (props) => {
 		</div>
 	);
 };
-//
-// const mockPointsProps = {
-// 	rank: 7,
-// 	honor: 455,
-// 	completedChallenge: 23,
-// };
-//
-// const mockLanguagesProps = {
-// 	languagesTrained: 4,
-// 	highestTrained: 'JS',
-// 	mostRecent: 'JS',
-// };
-//
-// const mockHonorBreakdownProps = {
-// 	completedChallengeDone: 5,
-// 	completedChallengeTotal: 10,
-// 	authoredChallengeDone: null,
-// 	authoredChallengeTotal: 0,
-// 	commentsDone: null,
-// 	commentsTotal: 0,
-// 	referralsDone: null,
-// 	referralsTotal: 0,
-// 	achievementsDone: 8,
-// 	achievementsTotal: 9,
-// };
-//
-// const mockRankBreakDownProps = {
-// 	rankProgress: 25,
-// 	rank: 8,
-// };
-//
-// const mockCommunityProps = {
-// 	comments: 3,
-// 	collections: 2,
-// 	transactions: 12,
-// };
