48210d6413fb0ec0d8e95b9307a9ddda960c9da0
INTJT <intjtlmao@gmail.com>
2021-08-03 10:44:47 +0300

    remove checkConfig and add getEnv helper

--- server/src/helpers/index.ts
+++ server/src/helpers/index.ts
@@ -1,4 +1,4 @@
-export * from './check-config';
 export * from './errors';
 export * from './crypt';
 export * from './token';
+export * from './env.helper';
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Router } from 'express';
 import cors from 'cors';
 import passport from 'passport';
-import { checkConfig } from './helpers';
 import { ENV } from './common';
 import { initApi } from './api';
 import { errorHandlerMiddleware } from './middleware';
@@ -10,14 +9,13 @@ import 'reflect-metadata';
 import './data/db/connection';
 import './config/passport';
 
-checkConfig();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
-app.use(ENV.APP.API_PATH, initApi(Router));
+app.use(ENV.APP.API_PATH, initApi());
 app.use(errorHandlerMiddleware);
 
 app.listen(ENV.APP.PORT, () => {
