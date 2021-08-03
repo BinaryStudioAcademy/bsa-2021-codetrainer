94045bde133529187fa9d564812fceb7cd8e4812
Paul Vasylenko <maincrafter1307@gmail.com>
2021-08-03 10:41:23 +0300

    containers structure

--- client/src/containers/profile/index.tsx
+++ client/src/containers/profile/index.tsx
@@ -5,20 +5,49 @@ import { mockProfileBioProps, statsProps } from './mocks';
 import { useDispatch, useSelector } from 'react-redux';
 import { IRootState } from 'typings/root-state';
 import * as actions from './logic/actions';
+import { useCallback } from 'react';
+import { ActiveTabId } from './logic/models';
+import { profilePageTabs } from './config';
+import { useMemo } from 'react';
 
 export const Profile: React.FC = () => {
-	const activeTab = useSelector((state: IRootState) => state.profile.activeTab);
-	const tabContent = activeTab === 'stats' ? () => <Stats {...statsProps} /> : () => null;
+	const activeTabId = useSelector((state: IRootState) => state.profile.activeTab);
+	const getTabContent = useCallback((): React.ReactNode => {
+		switch (activeTabId) {
+			case ActiveTabId.Stats:
+				return <Stats statsInfo={statsProps} />;
+			default:
+				return <div>Page is not finished yet</div>;
+		}
+	}, [activeTabId]);
 	const dispatch = useDispatch();
-	const setActiveTab = (tab: string) => {
-		dispatch(actions.setActiveTab({ activeTab: tab }));
-	};
+	const setActiveTab = useCallback(
+		(tabId: ActiveTabId) => {
+			dispatch(actions.setActiveTab({ activeTab: tabId }));
+		},
+		[dispatch],
+	);
+
+	const tabItems = useMemo(() => {
+		return profilePageTabs.map((item) => {
+			return {
+				tabid: item.id,
+				tabNameText: item.name,
+				onClick: () => setActiveTab(item.id),
+			};
+		});
+	}, [setActiveTab]);
+
 	return (
 		<ProfilePage
 			userInfo={mockProfileBioProps}
-			activeTab={activeTab}
-			tabContent={tabContent}
-			setActiveTab={setActiveTab}
+			profileInfoProps={{
+				getTabContent,
+				profileRouteProps: {
+					tabItems,
+					activeTabId,
+				},
+			}}
 		/>
 	);
 };
--- client/src/containers/profile/tabs/stats/index.tsx
+++ client/src/containers/profile/tabs/stats/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { StatsTab } from '../../../../components';
+import { StatsTab } from 'components';
 import { IStatsProps } from 'components/pages/profile/profile-info/tabs/stats';
 
-export const Stats: React.FC<IStatsProps> = (props) => {
-	return <StatsTab {...props} />;
+interface IStatsContainerProps {
+	statsInfo: IStatsProps;
+}
+
+export const Stats: React.FC<IStatsContainerProps> = ({ statsInfo }) => {
+	return <StatsTab {...statsInfo} />;
 };
