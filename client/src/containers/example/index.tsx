4fd0244466a183ee95fd4d31bc9520096624a327
INTJT <intjtlmao@gmail.com>
2021-08-03 11:46:05 +0300

    add example

--- client/src/containers/example/index.tsx
+++ client/src/containers/example/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import styles from './example.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import * as actions from './logic/actions';
 import { IRootState } from 'typings/root-state';
+import { uploadImage } from 'services/images.service';
+import styles from './example.module.scss';
+import { useState } from 'react';
 
 const Example: React.FC = () => {
 	const dispatch = useDispatch();
 	const text = useSelector((rootState: IRootState) => rootState.example.name);
+	const [file, setFile] = useState<Blob | null>(null);
 	const getExampleText = (exampleName: string) => {
 		dispatch(actions.getExampleText({ exampleName }));
 	};
@@ -20,6 +23,23 @@ const Example: React.FC = () => {
 				get second text
 			</button>
 			<p>{text}</p>
+			<form
+				method="POST"
+				onSubmit={async (event) => {
+					event.preventDefault();
+					await uploadImage(file as Blob);
+				}}
+			>
+				<input
+					type="file"
+					required
+					onChange={(event) => {
+						setFile(event.target.files && event.target.files[0]);
+					}}
+				/>
+				<input type="submit" />
+			</form>
+			<img src="api/images/0b5f991b-67cd-4991-b2bc-a704d5ea0d56" />
 		</div>
 	);
 };
