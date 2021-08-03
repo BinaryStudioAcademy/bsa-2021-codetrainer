74d596c4538b68be53a3c7ffced5a29d20a7247b
INTJT <intjtlmao@gmail.com>
2021-08-03 11:30:10 +0300

    remove comments in passport config

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,15 +1,8 @@
 import passport from 'passport';
 import { getCustomRepository } from 'typeorm';
 import { Strategy as LocalStrategy } from 'passport-local';
-// import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-// import { ENV } from '../common';
 import { UserRepository } from '../data';
 import { cryptCompare } from '../helpers';
-
-// const options = {
-// 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-// 	secretOrKey: ENV.JWT.SECRET,
-// };
 
 passport.use(
 	'login',
@@ -47,15 +40,3 @@ passport.use(
 		},
 	),
 );
-
-// passport.use(
-// 	new JwtStrategy(options, async ({ id }, done) => {
-// 		const repository = getCustomRepository(UserRepository);
-// 		try {
-// 			const user = await repository.getById(id);
-// 			return user ? done(null, user) : done({ status: 401, message: 'Token is invalid.' }, null);
-// 		} catch (err) {
-// 			return done(err);
-// 		}
-// 	}),
-// );
