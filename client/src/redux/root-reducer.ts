a6e928d3b5a58e76eebe72dc7181d627f7d69814
Vlad Zusko <vladzvu11@gmail.com>
2021-08-05 21:51:45 +0300

    Added some redux basics

--- client/src/redux/root-reducer.ts
+++ client/src/redux/root-reducer.ts
@@ -1,11 +1,13 @@
 import { exampleReducer } from 'containers/example/logic/reducer';
 import { profileReducer } from 'containers/profile/logic/reducer';
+import { clansReducer } from 'containers/clans/logic/reducer';
 import { combineReducers, Reducer } from 'redux';
 import { IRootState } from 'typings/root-state';
 
 const rootReducer: Reducer<IRootState> = combineReducers({
 	example: exampleReducer,
 	profile: profileReducer,
+	clans: clansReducer,
 });
 
 export default rootReducer;
--- client/src/redux/root-saga.ts
+++ client/src/redux/root-saga.ts
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import exampleSaga from 'containers/example/logic/saga';
+import clansSaga from 'containers/clans/logic/saga';
 
 export default function* rootSaga() {
-	yield all([exampleSaga()]);
+	yield all([exampleSaga(), clansSaga()]);
 }
