bfa16b1de20b57dc7ad8b44a89eb2b22559c524c
Mir <mir_slav@i.ua>
2021-08-05 18:23:19 +0300

    change white-routes

--- server/src/common/constants/helpers/errors/code-errors.ts
+++
@@ -1,5 +0,0 @@
-export const CODE_ERRORS = {
-	TOKEN_VERIFY: { message: 'Token verify error', status: 401 },
-	TOKEN_EXPIRED: { message: 'Token expired', status: 401 },
-	TOKEN_INVALID: { message: 'Invalid token', status: 401 },
-};
