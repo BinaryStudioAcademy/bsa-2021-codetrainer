90f3d6c97bb45458af5ed36f4e9c14b58a32bb54
Vlad Zusko <vladzvu11@gmail.com>
2021-08-05 21:50:43 +0300

    Added basic root state adaptation

--- client/src/typings/root-state.ts
+++ client/src/typings/root-state.ts
@@ -1,7 +1,9 @@
 import { IProfileState } from './../containers/profile/logic/state';
 import { IExampleState } from 'containers/example/logic/state';
+import { IClansState } from 'containers/clans/logic/state';
 
 export interface IRootState {
 	example: IExampleState;
 	profile: IProfileState;
+	clans: IClansState;
 }
