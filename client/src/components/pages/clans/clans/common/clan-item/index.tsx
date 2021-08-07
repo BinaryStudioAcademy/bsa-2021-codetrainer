468acc65cf8513f238b1fb0cc7e3e887740f8a5b
Vlad Zusko <vladzvu11@gmail.com>
2021-08-07 23:44:24 +0300

    Added clans page

---
+++ client/src/components/pages/clans/clans/common/clan-item/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './clan-item.module.scss';
+import { Rank } from 'components';
+import { IClan } from 'containers/clans/types';
+import { Button } from 'components/basic';
+
+const ClanItem: React.FC<{ clan: IClan }> = ({ clan }) => {
+	return (
+		<tr className={styles.clanItem}>
+			<td>
+				<Rank rank={clan.rank} />
+			</td>
+			<td>
+				{clan.avatar ? <img className={styles.clanItemAvatar} src={clan.avatar} /> : <span>No avatar</span>}
+			</td>
+			<td>
+				<span className={styles.clanItemName}>{clan.name}</span>
+			</td>
+			<td>
+				<span className={styles.clanItemSize}>{clan.maxMembers}</span>
+			</td>
+			<td>
+				<span className={styles.clanItemCreationDate}>
+					{clan.createdAt.toLocaleString('en-us', { month: 'long' })} {clan.createdAt.getFullYear()}
+				</span>
+			</td>
+			<td>
+				<span className={styles.clanItemHonour}>{clan.honour}</span>
+			</td>
+			<td>
+				<Button>Join</Button>
+			</td>
+		</tr>
+	);
+};
+
+export default ClanItem;
