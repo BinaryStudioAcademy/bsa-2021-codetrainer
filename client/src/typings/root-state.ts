eb3bfbcfea4fa666ab37ca81fd15e90976d4dc8d
Vlad Zusko <vladzvu11@gmail.com>
2021-08-07 23:38:47 +0300

    Updated root store type

--- client/src/typings/root-state.ts
+++ client/src/typings/root-state.ts
@@ -1,9 +1,11 @@
 import { IProfileState } from './../containers/profile/logic/state';
 import { IExampleState } from 'containers/example/logic/state';
-import { IClansState } from 'containers/clans/logic/state';
+import { IClansState } from 'containers/clans/clans/logic/state';
+import { IClanState } from 'containers/clans/clan/logic/state';
 
 export interface IRootState {
 	example: IExampleState;
 	profile: IProfileState;
 	clans: IClansState;
+	clan: IClanState;
 }
