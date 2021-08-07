f4f968c4788aa47a40dac41667ceb071534e063a
Vlad Zusko <vladzvu11@gmail.com>
2021-08-07 23:50:52 +0300

    Fixed clan action name

--- client/src/containers/clans/clan/logic/action-types.ts
+++ client/src/containers/clans/clan/logic/action-types.ts
@@ -1,7 +1,7 @@
 import { IClan } from '../../types';
 
 export const FETCH_CLAN = 'CLAN:FETCH_CLAN';
-export const SET_CLAN = 'CLAN:FETCH_CLANS';
+export const SET_CLAN = 'CLAN:SET_CLAN';
 export const LEAVE_CLAN = 'CLAN:LEAVE_CLAN';
 export const SORT_MEMBERS_BY_TIME = 'CLAN:SORT_MEMBERS_BY_TIME';
 export const SORT_MEMBERS_BY_RANK = 'CLAN:SORT_MEMBERS_BY_RANK';
