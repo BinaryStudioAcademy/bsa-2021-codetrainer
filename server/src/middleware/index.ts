56d2a588326667b633a894b2b6835f34cc1ae0d4
Vlad Zusko <vladzvu11@gmail.com>
2021-08-07 18:54:49 +0300

    Added middlewares to index file

--- server/src/middleware/index.ts
+++ server/src/middleware/index.ts
@@ -1,6 +1,6 @@
 export * from './registration';
 export * from './authentication';
 export * from './error-handler';
-export * from './check_clan';
+export * from './clan-permission';
 export * from './authorization';
 export * from './data-validation/data-validation';
