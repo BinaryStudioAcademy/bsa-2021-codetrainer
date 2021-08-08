a70863cd40b19b970eb1ce1dd0554c34e46fcc90
Taras Dubyk <taras.dubyk@binary-studio.com>
2021-08-08 19:30:07 +0300

    fix cookies

--- server/src/config/cookie-session/cookie-config.ts
+++ server/src/config/cookie-session/cookie-config.ts
@@ -1,11 +1,11 @@
 import ms from 'ms';
 import { ENV } from '../../common';
 
-export const cookieConfig = {
+export const cookieConfig: CookieSessionInterfaces.CookieSessionOptions = {
 	name: ENV.COOKIE.NAME,
 	keys: [ENV.COOKIE.SECRET] as Array<string>,
-	cookie: {
-		maxAge: ms(ENV.COOKIE.MAX_AGE),
-		httpOnly: ENV.COOKIE.HTTP_ONLY,
-	},
+	maxAge: ms(ENV.COOKIE.MAX_AGE),
+	httpOnly: ENV.COOKIE.HTTP_ONLY,
+	sameSite: 'none',
+	secure: true,
 };
