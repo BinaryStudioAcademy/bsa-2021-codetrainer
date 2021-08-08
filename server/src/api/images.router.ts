9e7a27426b6879334ca492f5a9bbd6b9b0e75e98
INTJT <intjtlmao@gmail.com>
2021-08-08 21:31:35 +0300

    rename image controller to router
    actually it's controller + router, but who cares?

---
+++ server/src/api/images.router.ts
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from 'express';
+import { HttpCodes } from '../common/enum/http-codes';
+import { imageMiddleware } from '../middleware';
+import { ImagesService } from '../services';
+
+export function imagesRouter(service: ImagesService): Router {
+	const route = Router();
+
+	route.post('/upload', imageMiddleware, async (req: Request, res: Response) => {
+		const image = req.file;
+		if (image) {
+			const href = await service.putImage(image);
+			res.status(HttpCodes.OK).json({ href });
+		} else {
+			res.status(HttpCodes.BAD_REQUEST).json({ message: 'No image' });
+		}
+	});
+
+	return route;
+}
