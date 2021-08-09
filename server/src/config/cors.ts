47363b0d2cd1abc38c88b8f6d8f3bfe080cf9de6
Mir <mir_slav@i.ua>
2021-08-09 13:28:45 +0300

    fix cors

--- server/src/config/cors.ts
+++ server/src/config/cors.ts
@@ -1,14 +1,14 @@
 import { CorsOptions } from 'cors';
-import { CORS_ORIGIN_URLS, STAGING_URL, CORS_METHODS, CORS_CREDENTIALS } from '../common';
+import { CORS_ORIGIN_URLS, CORS_METHODS, CORS_CREDENTIALS } from '../common';
 import { cookieConfig } from './cookie-session/cookie-config';
 
 export const corsConfig: CorsOptions = {
 	origin: (origin, callback) => {
-		if (CORS_ORIGIN_URLS.includes(origin as string)) {
-			if (origin !== STAGING_URL) {
-				delete cookieConfig.sameSite;
-				delete cookieConfig.secure;
-			}
+		if (CORS_ORIGIN_URLS.includes(origin as string) || !origin) {
+			// if (origin !== STAGING_URL) {
+			// 	delete cookieConfig.sameSite;
+			// 	delete cookieConfig.secure;
+			// }
 			callback(null, true);
 		} else {
 			callback(new Error('Not allowed by CORS'));
