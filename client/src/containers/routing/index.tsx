37fa617e5fe4940d07cfa9e8a10fc47a8b55fc6d
AlexanderKhapchenko <alexanderfox1337@gmail.com>
2021-08-09 03:25:01 +0300

    added setting page container

--- client/src/containers/routing/index.tsx
+++ client/src/containers/routing/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Example from 'containers/example';
 import HomePage from 'containers/home-page';
+import SettingPage from 'containers/setting-page';
 import { Profile } from 'containers/profile';
 import { PrivateRoute, PublicRoute, ForgotPassword, ChangePassword, SignUp, SearchPage } from 'components';
 import SignIn from 'containers/sign-in';
@@ -13,6 +14,7 @@ const Routing: React.FC<IRoutingProps> = () => (
 		<PublicRoute exact restricted={false} path="/" component={Example} />
 		<PrivateRoute path="/home" component={HomePage} />
 		<PrivateRoute path="/users/:name" component={Profile} />
+		<PrivateRoute path="/setting" component={SettingPage} />
 		<PublicRoute exact restricted={false} path="/register" component={SignUp} />
 		<PublicRoute exact restricted={false} path="/sign-in" component={SignIn} />
 		<PublicRoute exact restricted={false} path="/forgot-password" component={ForgotPassword} />
