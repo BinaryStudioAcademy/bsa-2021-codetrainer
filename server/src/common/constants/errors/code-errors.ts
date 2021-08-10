65f8cae77ed8bf63f5e546b53a9fe5d6c73435ca
Mir <mir_slav@i.ua>
2021-08-10 20:42:41 +0300

    add info to code-error

--- server/src/common/constants/errors/code-errors.ts
+++ server/src/common/constants/errors/code-errors.ts
@@ -1,7 +1,10 @@
+import { HttpCodes } from '../../enum';
+
 export const CODE_ERRORS = {
 	CLAN_NAME_IS_TAKEN: (name: string) => ({ message: `Clan name: ${name} is already taken.`, status: 401 }),
 	NO_CLAN: { message: 'You have no clan.', status: 401 },
 	CLAN_NOT_EXIST: (id: string) => ({ message: `Clan with id ${id} does not exist.`, status: 401 }),
+	CLAN_NOT_PERMISSION: { message: 'no permission', status: HttpCodes.FORBIDDEN },
 	IN_CLAN: { message: 'You are already in clan.', status: 401 },
 	NOT_IN_CLAN: { message: 'You are not in clan.', status: 401 },
 	ADMIN_LEAVE: { message: 'Admin can not leave his clan.', status: 401 },
--- server/src/middleware/check/clan/clan-permission/admin-permission-middleware.ts
+++ server/src/middleware/check/clan/clan-permission/admin-permission-middleware.ts
@@ -1,11 +1,11 @@
 import { RequestHandler } from 'express';
-import { CLAN_MEMBER_ROLE } from '../../common';
-import { ValidationError } from '../../helpers';
+import { CLAN_MEMBER_ROLE, CODE_ERRORS } from '../../../../common';
+import { ValidationError } from '../../../../helpers';
 
 const adminPermissionMiddleware: RequestHandler = (req, _response, next) => {
 	const { user } = req;
 	if (user.profileClan?.role !== CLAN_MEMBER_ROLE.ADMIN) {
-		throw new ValidationError({ message: 'no permission', status: 401 });
+		throw new ValidationError(CODE_ERRORS.CLAN_NOT_PERMISSION);
 	}
 	next();
 };
