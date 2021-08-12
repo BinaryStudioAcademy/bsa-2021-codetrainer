8b1d67386d6c351bb63cd696a1418f97fd7a1321
Vlad Zusko <vladzvu11@gmail.com>
2021-08-12 12:31:23 +0300

    Fixed join/left methods of service

--- server/src/services/clan/clan-service.ts
+++ server/src/services/clan/clan-service.ts
@@ -1,9 +1,7 @@
 import { getCustomRepository } from 'typeorm';
 import { Clan, TClanRepository, TProfileClanRepository, TUserRepository, User } from '../../data';
 import { ValidationError } from '../../helpers';
-import { Clan as ClanType } from '../../data/models';
 import { CLAN_IS_PUBLIC, CLAN_MAX_MEMBERS, CLAN_MEMBER_ROLE, CLAN_MEMBER_STATUS, CODE_ERRORS } from '../../common';
-import { IUserFields } from '../../types';
 
 export class ClanService {
 	protected clanRepository: TClanRepository;
@@ -79,23 +77,18 @@ export class ClanService {
 		return newClan;
 	}
 
-	getHonour({ members }: ClanType) {
+	getHonour({ members }: Clan) {
 		return +(members.reduce((sum, member) => sum + member.honour, 0) / members.length).toFixed();
 	}
 
-	getRank({ members }: ClanType) {
+	getRank({ members }: Clan) {
 		return +(members.reduce((sum, member) => sum + member.rank, 0) / members.length).toFixed();
 	}
 
 	async getClans({ skip = 0, take = 10 }) {
 		const repository = getCustomRepository(this.clanRepository);
 		const clans = (await repository.getAll(skip, take)).map((clan) => ({
-			id: clan.id,
-			name: clan.name,
-			isPublic: clan.isPublic,
-			maxMembers: clan.maxMembers,
-			numberOfMembers: clan.numberOfMembers,
-			createdAt: clan.createdAt,
+			...clan,
 			honour: this.getHonour(clan),
 			rank: this.getRank(clan),
 		}));
@@ -118,8 +111,10 @@ export class ClanService {
 		};
 	}
 
-	async joinClan(user: IUserFields, id: string) {
+	async joinClan(user: User, id: string) {
 		const repository = getCustomRepository(this.clanRepository);
+		const userRepository = getCustomRepository(this.userRepository);
+		const profileClanRepository = getCustomRepository(this.profileClanRepository);
 		const clan = await this.getClan(id);
 
 		if (!clan) {
@@ -130,18 +125,25 @@ export class ClanService {
 			throw new ValidationError(CODE_ERRORS.IN_CLAN);
 		}
 
+		const profileClan = await profileClanRepository.save({
+			role: CLAN_MEMBER_ROLE.MEMBER,
+			status: CLAN_MEMBER_STATUS.APPROVED,
+		});
+
+		await userRepository.updateById(user.id, { profileClan });
 		await repository.addMember(id, user.id);
 	}
 
-	async leaveClan(user: IUserFields, id: string) {
+	async leaveClan(user: User, id: string) {
 		const repository = getCustomRepository(this.clanRepository);
+		const userRepository = getCustomRepository(this.userRepository);
 		const clan = await this.getClan(id);
 
 		if (!clan) {
 			throw new ValidationError(CODE_ERRORS.NOT_EXIST(id));
 		}
 
-		if (user.clan.id !== id) {
+		if (user.clan?.id !== id) {
 			throw new ValidationError(CODE_ERRORS.NOT_IN_CLAN);
 		}
 
@@ -149,10 +151,11 @@ export class ClanService {
 			throw new ValidationError(CODE_ERRORS.ADMIN_LEAVE);
 		}
 
+		await userRepository.updateById(user.id, { profileClan: undefined });
 		await repository.deleteMember(id, user.id);
 	}
 
-	async toggleMember(user: IUserFields, id: string) {
+	async toggleMember(user: User, id: string) {
 		const clan = await this.getClan(id);
 
 		if (!clan) {
