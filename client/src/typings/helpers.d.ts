9b744394447cffaaa7ec5774ce6998a087bec7a7
Vlad Zusko <vladzvu11@gmail.com>
2021-08-12 15:51:06 +0300

    Added missed PATCH method for request

--- client/src/typings/helpers.d.ts
+++ client/src/typings/helpers.d.ts
@@ -1,8 +1,8 @@
 declare namespace Helpers {
 	export interface IRequestArgs {
 		endpoint: string;
-		method: 'GET' | 'POST' | 'PUT' | 'DELETE';
 		skipAuthorization?: boolean;
+		method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
 		query?: Record<string, any>;
 		body?: any;
 	}
