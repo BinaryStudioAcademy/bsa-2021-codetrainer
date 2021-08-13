3dcdeef7071ef42a998a9ac75fb576b21bc52369
Vlad Zusko <vladzvu11@gmail.com>
2021-08-13 10:13:13 +0300

    Removed API models namespace

--- client/src/typings/sign-in-form.ts
+++ client/src/typings/sign-in-form.ts
@@ -2,12 +2,3 @@ export interface ISignInForm {
 	email: string;
 	password: string;
 }
-
-export interface IUser {
-	id: string;
-	name: string;
-	surname: string;
-	email: string;
-	clan?: string;
-	profileClan?: string;
-}
