a7c12889d5d7e020f93eb1c601206a0a6abe5f48
Vlad Zusko <vladzvu11@gmail.com>
2021-08-13 13:34:23 +0300

    Added visiting clan functionality

--- client/src/components/pages/clans/clans/components/clan-item/index.tsx
+++ client/src/components/pages/clans/clans/components/clan-item/index.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
+import { useHistory } from 'react-router';
 import { Rank } from 'components';
 import { IClanItemProps } from './types';
 import { Button } from 'components/basic';
 import { MemberRoles } from 'common/enum/app/clans';
 import { ButtonClasses } from 'components/basic/button';
 import { getMonthName } from 'helpers/date';
+import { ROUTES } from 'constants/routes';
+import styles from './clan-item.module.scss';
 
 const ClanItem: React.FC<IClanItemProps> = ({ clan, userId, joinClan, leaveClan }) => {
+	const history = useHistory();
 	const currentUserMember = clan.members.find((member) => member.id === userId);
 
+	const goToClanHanler = (event: MouseEvent<HTMLElement>) => {
+		event.stopPropagation();
+
+		if (currentUserMember) {
+			history.push(ROUTES.Clan);
+		}
+	};
+
+	const leaveClanHanler = (event: MouseEvent<HTMLElement>) => {
+		event.stopPropagation();
+		leaveClan(clan.id);
+	};
+
+	const joinClanHanler = (event: MouseEvent<HTMLElement>) => {
+		event.stopPropagation();
+		joinClan(clan.id);
+	};
+
 	return (
-		<tr>
+		<tr onClick={goToClanHanler} className={currentUserMember && styles.clanJoined}>
 			<td>
 				<Rank rank={clan.rank} />
 			</td>
@@ -32,12 +54,12 @@ const ClanItem: React.FC<IClanItemProps> = ({ clan, userId, joinClan, leaveClan 
 			<td>
 				{currentUserMember ? (
 					currentUserMember.profileClan?.role !== MemberRoles.ADMIN ? (
-						<Button className={ButtonClasses.red} onClick={() => leaveClan(clan.id)}>
+						<Button className={ButtonClasses.red} onClick={leaveClanHanler}>
 							Leave
 						</Button>
 					) : null
 				) : (
-					<Button onClick={() => joinClan(clan.id)}>Join</Button>
+					<Button onClick={joinClanHanler}>Join</Button>
 				)}
 			</td>
 		</tr>
