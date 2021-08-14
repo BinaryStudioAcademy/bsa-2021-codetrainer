9348576029d323bf23a576c95e264a0ab69ab2cc
INTJT <intjtlmao@gmail.com>
2021-08-14 22:08:04 +0300

    add helpers

---
+++ client/src/helpers/user.helper.ts
@@ -0,0 +1,16 @@
+import { getProfile } from 'services/github.service';
+import { IUser } from 'typings/common/IUser';
+
+export async function mapUserResponseToUser({ githubId, ...remains }: WebApi.Entities.IUser): Promise<IUser> {
+	try {
+		const githubProfile = githubId ? await getProfile(githubId) : undefined;
+		return {
+			...remains,
+			github: githubProfile,
+		};
+	} catch {
+		return {
+			...remains,
+		};
+	}
+}
