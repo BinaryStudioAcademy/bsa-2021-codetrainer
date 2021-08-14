4ffae51829394740a14699690b3c3f237c7c060d
INTJT <intjtlmao@gmail.com>
2021-08-14 21:24:24 +0300

    add githubRedirectMiddleware

---
+++ server/src/middleware/github-redirect.middleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { HttpCodes } from '../common/enum/http-codes';
+import { ENV } from '../common';
+
+// passport-github isn't the perfect solution
+// GithubStrategy set 302 status code so it's impossible to handle this in fetch
+
+function generateGithubLink({ callback, scope }: { callback: string; scope?: string[] }) {
+	const query = new URLSearchParams({
+		client_id: ENV.GITHUB.CLIEND_ID,
+		redirect_uri: ENV.GITHUB.CALLBACK + callback,
+	});
+	if (scope && scope.length !== 0) {
+		query.append('scope', scope.join(' '));
+	}
+	return `https://github.com/login/oauth/authorize?${query.toString()}`;
+}
+
+export function githubRedirectMiddleware({ callback, scope = [] }: { callback: string; scope?: string[] }) {
+	const link = generateGithubLink({ callback, scope });
+	return (_req: Request, res: Response) => {
+		res.status(HttpCodes.OK).json({ link });
+	};
+}
