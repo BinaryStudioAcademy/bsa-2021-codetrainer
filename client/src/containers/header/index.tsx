7412aa68a4bae9d756fb834b83e460d45c7b02dc
INTJT <intjtlmao@gmail.com>
2021-08-18 19:23:35 +0300

    add notifications to header container

--- client/src/containers/header/index.tsx
+++ client/src/containers/header/index.tsx
@@ -1,8 +1,9 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import { Header as HeaderComponent } from 'components';
 import * as actions from '../user/logic/actions';
-import { useDispatch } from 'react-redux';
-import { useAppSelector } from 'hooks/useAppSelector';
+import * as notificationActions from './notifications/logic/actions';
+import { useHeaderSelector, useUserSelector } from 'hooks/useAppSelector';
 import { IHeaderProps } from 'components/common/header/index';
 import menuProfile from 'assets/icons/header/menu/avatar.svg';
 import menuSettings from 'assets/icons/header/menu/settings.svg';
@@ -11,52 +12,55 @@ import menuSignout from 'assets/icons/header/menu/logout.svg';
 import { ROUTES } from 'constants/routes';
 import { useHistory } from 'react-router-dom';
 
-
-
 const Header = () => {
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const handleLogout = async () => {
-        dispatch(actions.logoutUser());
-    };
-    const { user } = useAppSelector((state) => state.auth.userData);
+	const history = useHistory();
+	const user = useUserSelector();
+	const dispatch = useDispatch();
+	const handleLogout = useCallback(() => {
+		dispatch(actions.logoutUser());
+	}, [user]);
+	const { notifications: unorderedNotifications } = useHeaderSelector();
 
-    const headerProps: IHeaderProps = useMemo(() => ({
-        name: `${user?.name} ${user?.surname}`,
-        rank: 7,
-        notificationCounter: 4,
-        mark: 400,
-        listItems: [
-            {
-                id: '1',
-                image: menuProfile,
-                text: 'View Profile',
-                onClick: () => history.push(ROUTES.UserProfile),
-            },
-            {
-                id: '2',
-                image: menuSettings,
-                text: 'Account Settings',
-                onClick: () => history.push(ROUTES.Setting),
-            },
-            {
-                id: '3',
-                image: menuChallenge,
-                text: 'New Challenge',
-                onClick: () => history.push(ROUTES.createTask),
-            },
-            {
-                id: '4',
-                image: menuSignout,
-                text: 'Sign out',
-                onClick: handleLogout
-            },
-        ],
-    }), [history, handleLogout]);
+	const headerProps: IHeaderProps = useMemo(
+		() => ({
+			name: `${user?.name} ${user?.surname}`,
+			rank: 7,
+			mark: 400,
+			listItems: [
+				{
+					id: '1',
+					image: menuProfile,
+					text: 'View Profile',
+					onClick: () => history.push(ROUTES.UserProfile),
+				},
+				{
+					id: '2',
+					image: menuSettings,
+					text: 'Account Settings',
+					onClick: () => history.push(ROUTES.Setting),
+				},
+				{
+					id: '3',
+					image: menuChallenge,
+					text: 'New Challenge',
+					onClick: () => history.push(ROUTES.createTask),
+				},
+				{
+					id: '4',
+					image: menuSignout,
+					text: 'Sign out',
+					onClick: handleLogout,
+				},
+			],
+			notifications: Array.from(unorderedNotifications.values()).sort((a, b) => +b.date - +a.date),
+			onReadNotification(id: string) {
+				dispatch(notificationActions.readNotification({ id }));
+			},
+		}),
+		[history, handleLogout, unorderedNotifications],
+	);
 
-    return (
-        <HeaderComponent {...headerProps} />
-    );
+	return <HeaderComponent {...headerProps} />;
 };
 
 export default Header;
