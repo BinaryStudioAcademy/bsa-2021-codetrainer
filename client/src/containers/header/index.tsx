87ec27ab8f73d02b31f0f0ac79eb10298d974a18
INTJT <74379701+INTJT@users.noreply.github.com>
2021-08-20 21:45:18 +0300

    move bell to assets

--- client/src/containers/header/index.tsx
+++ client/src/containers/header/index.tsx
@@ -1,10 +1,13 @@
-import React, from 'react';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import * as notificationsActions from './notifications/logic/actions';
 import { Header as HeaderComponent } from 'components';
 import { useAppSelector, useHeaderSelector } from 'hooks/useAppSelector';
 import { IHeaderProps } from 'components/common/header/index';
 import { getListItems } from './items-dropdown';
 
 const Header: React.FC = () => {
+	const dispatch = useDispatch();
 	const { user } = useAppSelector((state) => state.auth.userData);
 	const { notifications: unorderedNotifications } = useHeaderSelector();
 
@@ -14,6 +17,7 @@ const Header: React.FC = () => {
 		notifications: Array.from(unorderedNotifications.values()).sort((a, b) => +b.date - +a.date),
 		mark: 400,
 		listItems: getListItems(),
+		onReadNotification: id => dispatch(notificationsActions.readNotification({ id }))
 	};
 
 	return <HeaderComponent {...headerProps} />;
