ce1c758362f5f618e6651a398a5d92aa47e27b70
Vlad Zusko <vladzvu11@gmail.com>
2021-08-21 11:35:08 +0300

    Added small task service fix

--- server/src/services/task/task-service.ts
+++ server/src/services/task/task-service.ts
@@ -4,8 +4,11 @@ import { TASK_ORDER_BY, TASK_STATUS } from '../../common';
 
 export class TaskService {
 	protected taskRepository: TTaskRepository;
+
 	protected userRepository: TUserRepository;
+
 	protected tagRepository: TTagRepository;
+
 	protected getTagBind: (name: string) => Promise<Tag>;
 
 	constructor({ task, user, tag }: { task: TTaskRepository; user: TUserRepository; tag: TTagRepository }) {
@@ -24,7 +27,7 @@ export class TaskService {
 		return tag;
 	}
 
-	async create(user: User, task: Task, tags: string[] | []) {
+	async create(user: User, task: Task, tags: string[] = []) {
 		const repository = getCustomRepository(this.taskRepository);
 		const userRepository = getCustomRepository(this.userRepository);
 		const tagsForSave = await Promise.all(tags.map(this.getTagBind));
@@ -33,7 +36,7 @@ export class TaskService {
 			user,
 			isPublished: false,
 			status: TASK_STATUS.BETA,
-			...(Boolean(tagsForSave.length) ? { tags: tagsForSave } : {}),
+			...(tagsForSave.length ? { tags: tagsForSave } : {}),
 		});
 		await userRepository.save({ id: user.id, tasks: [...user.tasks, newTask] });
 		const savedTask = await repository.getById(newTask.id);
