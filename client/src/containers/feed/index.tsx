564b4615f9aa68faf786d686cf1ffa285c2161e0
dammmula <eliza04102001@gmail.com>
2021-08-22 13:06:54 +0300

    Add community members

--- client/src/containers/feed/index.tsx
+++ client/src/containers/feed/index.tsx
@@ -4,36 +4,6 @@ import { getMessages } from '../home-page/logic/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { IRootState } from '../../typings/root-state';
 
-const testMessages = {
-	messages: [
-		{
-			id: '01',
-			userImageSource: '',
-			userName: 'Angel Mango',
-			clan: 'Clan “Fiksiki”',
-			date: '2 hour ago',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in turpis sed vitae egestas nullam semper aenean. Neque id tortor, nibh netus viverra sed orci. Convallis faucibus tempor in vitae nulla lectus.',
-		},
-		{
-			id: '02',
-			userImageSource: '',
-			userName: 'Jaydon Passaquindici Arcand',
-			clan: 'Clan “Fighter”',
-			date: '2 hour ago',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis feugiat volutpat aliquet in. Morbi morbi sed neque metus, scelerisque enim molestie bibendum metus. Et cras venenatis nunc adipiscing cursus enim. Nullam velit arcu vitae in tincidunt fringilla nisi, magna amet.',
-		},
-		{
-			id: '03',
-			userImageSource: '',
-			userName: 'Allison Ekstrom Bothman',
-			clan: 'Clan “Mivina”',
-			date: '2 hour ago',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in turpis sed vitae egestas nullam semper.',
-		},
-	],
-	isLastPage: false,
-};
-
 export const FeedContainer = () => {
 	const dispatch = useDispatch();
 	const messages = useSelector((rootState: IRootState) => rootState.home.state.messages);
@@ -53,7 +23,7 @@ export const FeedContainer = () => {
 				messages={messages}
 				selectedFeedCategory={selectedFeedCategory}
 				onSelectFeedCategory={handleSelectFeedCategory}
-				isLastPage={testMessages.isLastPage}
+				isLastPage={false}
 			/>
 		</>
 	);
---
+++ client/src/services/followers.service.ts
@@ -0,0 +1,11 @@
+import { http } from 'services';
+
+export const fetchCommunity = async (id: string): Promise<Record<string, any>> => {
+	const community = await http.callWebApi({
+		method: 'GET',
+		endpoint: `followers/community/${id}`,
+	});
+	console.log(community);
+
+	return community;
+};
