f266db9c1a4d8f5f80176faef26c207858d36d18
dammmula <eliza04102001@gmail.com>
2021-08-23 13:52:23 +0300

    Add TaskInfo block

--- client/src/components/pages/task/task-info/index.tsx
+++ client/src/components/pages/task/task-info/index.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import styles from './task-info.module.scss';
+import { Challenge } from 'components';
+import { Button } from 'components/basic';
+import { ButtonClasses } from 'components/basic/button';
+import clsx from 'clsx';
 
 export const TaskInfo = () => {
+	const challengeProps = {
+		id: 'fdsafsa',
+		author: {
+			firstName: 'fdsa',
+			lastName: 'fdsafdsagfds',
+			link: '',
+		},
+		linkToAuthor: '',
+		title: 'title',
+		rank: 8,
+		stats: {
+			favoriteSaves: 23,
+			positiveFeedback: 88,
+		},
+		tags: ['fdsafdsa', 'fdsafdsewqrwe', 'terwtree'],
+	};
 	return (
 		<div className={styles.taskInfo}>
-			<div className={styles.infoBlock}></div>
-			<div className={styles.buttonsBlock}></div>
-			task info
+			<Challenge {...challengeProps} />
+			<div className={styles.buttonsBlock}>
+				<Button className={clsx(ButtonClasses.red, ButtonClasses.filled)}>Train</Button>
+				<Button className={clsx(ButtonClasses.red, styles.skipButton)}>Skip</Button>
+			</div>
 		</div>
 	);
 };
