e773e716a0aaa9bbc5f6a278b47792476598663f
dammmula <eliza04102001@gmail.com>
2021-08-24 12:39:28 +0300

    Add Contributors block

--- client/src/components/pages/task/tabs/detailsTab/index.tsx
+++ client/src/components/pages/task/tabs/detailsTab/index.tsx
@@ -1,20 +1,31 @@
+import { Avatar } from 'components/basic';
 import React from 'react';
 import styles from './details-tab.module.scss';
 
 export const DetailsTab = () => {
 	return (
-		<div>
-			<div className={styles.description}>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
-				</p>
-				<h3 className={styles.examplesHeader}>Examples</h3>
-				<code className={styles.code}>Lorem ipsum dolor sit amet,</code>
+		<>
+			<div>
+				<div className={styles.description}>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					<h3 className={styles.examplesHeader}>Examples</h3>
+					<code className={styles.code}>Lorem ipsum dolor sit amet,</code>
+				</div>
 			</div>
-		</div>
+			<div className={styles.contributors}>
+				<p>These users have contributed to this task:</p>
+				<div className={styles.users}>
+					<Avatar size={50} />
+					<Avatar size={50} />
+					<Avatar size={50} />
+				</div>
+			</div>
+		</>
 	);
 };
