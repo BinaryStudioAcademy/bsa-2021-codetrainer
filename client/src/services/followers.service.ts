a30c819079e7f4fe1938053de02aa560249cfbf7
dammmula <eliza04102001@gmail.com>
2021-08-25 12:52:17 +0300

    Add endpoint to constants

--- client/src/services/followers.service.ts
+++ client/src/services/followers.service.ts
@@ -1,9 +1,10 @@
 import { http } from 'services';
+import { ROUTES } from '../constants/routes';
 
 export const fetchCommunity = async (id: string): Promise<Record<string, any>> => {
 	const community = await http.callWebApi({
 		method: 'GET',
-		endpoint: `followers/community/${id}`,
+		endpoint: `${ROUTES.Community}/${id}`,
 	});
 
 	return community;
