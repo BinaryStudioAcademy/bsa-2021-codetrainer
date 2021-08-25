6506ac25a920741909ae9cf576dfe8c45b892dbd
Vlad Zusko <vladzvu11@gmail.com>
2021-08-25 09:42:26 +0300

    Added some icons

--- client/src/components/pages/landing/sections/header/navigation/index.jsx
+++ client/src/components/pages/landing/sections/header/navigation/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './navigation.module.scss';
+import logo from 'assets/images/logo.svg';
+import signInLogo from 'assets/icons/header/menu/logout.svg';
 
 const Navigation = () => {
 	return (
 		<nav className={styles.navigation}>
-			<div className={styles.navagationLogo}>
-				<img src="assets/images/logo.svg" alt="" />
+			<div className={styles.navigationLogo}>
+				<img src={logo} alt="" />
 			</div>
 			<div className={styles.navigationLinks}>
 				<NavLink to="/sign-in" className={styles.navigationLink}>
+					<img src={signInLogo} alt="" className={styles.navigationLinkLogo} />
 					Sign in
 				</NavLink>
 			</div>
