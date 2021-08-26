ffa0afe0467d2b36b70a8414a922f6490455cfa9
Vasylenko Paul <maincrafter1307@gmail.com>
2021-08-26 12:10:16 +0300

    deleted example

--- client/src/components/pages/not-found/index.tsx
+++ client/src/components/pages/not-found/index.tsx
@@ -5,43 +5,8 @@ import { ButtonClasses } from 'components/basic/button';
 import clsx from 'clsx';
 import { ROUTES } from 'constants/routes';
 import historyHelper from 'helpers/history.helper';
-import { TaskList } from 'components/common';
 
 const NotFound: React.FC = () => {
-	const tasks = [
-		{
-			id: '1',
-			linkToAuthor: '/',
-			author: {
-				firstName: 'A',
-				lastName: 'B',
-				link: '/',
-			},
-			stats: {
-				favoriteSaves: 12,
-				positiveFeedback: 12,
-			},
-			title: 'Title',
-			rank: 2,
-			tags: ['Tag 1', 'Tag 2'],
-		},
-		{
-			id: '2',
-			linkToAuthor: '/',
-			author: {
-				firstName: 'A',
-				lastName: 'B',
-				link: '/',
-			},
-			stats: {
-				favoriteSaves: 12,
-				positiveFeedback: 12,
-			},
-			title: 'Title',
-			rank: 2,
-			tags: ['Tag 1', 'Tag 2'],
-		},
-	];
 	return (
 		<>
 			<CoverLayout className={styles.container}>
@@ -57,7 +22,6 @@ const NotFound: React.FC = () => {
 					</Button>
 				</div>
 			</CoverLayout>
-			<TaskList title="Similar tasks" tasks={tasks} />
 		</>
 	);
 };
