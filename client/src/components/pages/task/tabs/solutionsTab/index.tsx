6b1aa6bd0faa2490a5ce4789f60578f3be390a4a
dammmula <eliza04102001@gmail.com>
2021-08-31 18:45:51 +0300

    Add solution component

--- client/src/components/pages/task/tabs/solutionsTab/index.tsx
+++ client/src/components/pages/task/tabs/solutionsTab/index.tsx
@@ -7,31 +7,79 @@ import { useState } from 'react';
 import { Description } from './description';
 import { TestCases } from './test-cases';
 import { WebApi } from 'typings/webapi';
+import { Solution } from './solution';
+import { Divider } from '@material-ui/core';
 
 export interface ISolutionsTabProps {
 	task: WebApi.Entities.IChallenge;
 }
 
+export enum ShowMe {
+	AllSolutions = 'All solutions',
+	SolutionsOfFollowing = 'Solutions of users I am following',
+}
+
+export enum SortBy {
+	Newest = 'Newest',
+	Oldest = 'Oldest',
+}
+
 export const SolutionsTab = ({ task }: ISolutionsTabProps) => {
 	const [isDescriptionOpened, setIsDescriptionOpened] = useState(false);
 	const [isTestCasesOpened, setIsTestCasesOpened] = useState(false);
+	const [activeShowMe, setActiveShowMe] = useState(ShowMe.AllSolutions);
+	const [activeSortBy, setActiveSortBy] = useState(SortBy.Newest);
+	const solutions = [
+		'fdsafdsafds',
+		'lorem ipsum',
+		'fkdsa ifundafa',
+		' jfldanu oieuafysdaoifnyceopoyfo ofiy y3tf ewwa',
+	];
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.filters}>
 				<div className={styles.showMe}>
 					<span className={styles.label}>Show me:</span>
-					<button className={clsx(styles.membersSortPanelButtonActive, styles.membersSortPanelButton)}>
-						All solutions
+					<button
+						onClick={() => setActiveShowMe(ShowMe.AllSolutions)}
+						className={clsx(
+							activeShowMe === ShowMe.AllSolutions && styles.membersSortPanelButtonActive,
+							styles.membersSortPanelButton,
+						)}
+					>
+						{ShowMe.AllSolutions}
 					</button>
-					<button className={clsx(styles.membersSortPanelButton)}>Solutions of users I am following</button>
+					<button
+						onClick={() => setActiveShowMe(ShowMe.SolutionsOfFollowing)}
+						className={clsx(
+							activeShowMe === ShowMe.SolutionsOfFollowing && styles.membersSortPanelButtonActive,
+							styles.membersSortPanelButton,
+						)}
+					>
+						{ShowMe.SolutionsOfFollowing}
+					</button>
 				</div>
 				<div className={styles.sortBy}>
 					<span className={styles.label}>Sort by:</span>
-					<button className={clsx(styles.membersSortPanelButtonActive, styles.membersSortPanelButton)}>
-						Newest
+					<button
+						onClick={() => setActiveSortBy(SortBy.Newest)}
+						className={clsx(
+							activeSortBy === SortBy.Newest && styles.membersSortPanelButtonActive,
+							styles.membersSortPanelButton,
+						)}
+					>
+						{SortBy.Newest}
 					</button>
-					<button className={clsx(styles.membersSortPanelButton)}>Oldest</button>
+					<button
+						onClick={() => setActiveSortBy(SortBy.Oldest)}
+						className={clsx(
+							activeSortBy === SortBy.Oldest && styles.membersSortPanelButtonActive,
+							styles.membersSortPanelButton,
+						)}
+					>
+						{SortBy.Oldest}
+					</button>
 				</div>
 			</div>
 			<div className={styles.toggleBtns}>
@@ -42,7 +90,11 @@ export const SolutionsTab = ({ task }: ISolutionsTabProps) => {
 					Show challenge description
 				</Button>
 				{isDescriptionOpened &&
-					(task.description ? <Description description={task.description} /> : 'No description provided')}
+					(task.description ? (
+						<Description description={task.description} />
+					) : (
+						<div className={styles.noInfoProvided}>No description provided</div>
+					))}
 				<Button
 					onClick={() => setIsTestCasesOpened(!isTestCasesOpened)}
 					className={clsx(ButtonClasses.blue, styles.skipButton)}
@@ -50,7 +102,22 @@ export const SolutionsTab = ({ task }: ISolutionsTabProps) => {
 					Show challenge test cases
 				</Button>
 				{isTestCasesOpened &&
-					(task.exampleTestCases ? <TestCases testCases={task.exampleTestCases} /> : 'No examples provided')}
+					(task.exampleTestCases ? (
+						<TestCases testCases={task.exampleTestCases} />
+					) : (
+						<div className={styles.noInfoProvided}>No examples provided</div>
+					))}
+			</div>
+
+			<div className={styles.solutions}>
+				{solutions.map((item) => {
+					return (
+						<>
+							<Solution solution={item} key={item} />
+							<Divider />
+						</>
+					);
+				})}
 			</div>
 		</div>
 	);
--- client/src/components/pages/task/tabs/solutionsTab/solution/index.tsx
+++ client/src/components/pages/task/tabs/solutionsTab/solution/index.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import styles from './solution.module.scss';
+import PeopleIcon from '@material-ui/icons/People';
+import ForumIcon from '@material-ui/icons/Forum';
+// import { Divider } from '@material-ui/core';
 
-export const Solution = () => {
-	return <div className={styles.solution}>solution</div>;
+export interface ISolutionProps {
+	solution: string;
+}
+
+export const Solution = ({ solution }: ISolutionProps) => {
+	return (
+		<div className={styles.solution}>
+			<div className={styles.header}>
+				{<PeopleIcon />}
+				<h3>User 4321421</h3>
+			</div>
+			<div className={styles.code}>
+				<code>{solution}</code>
+			</div>
+			<div className={styles.footer}>
+				<div className={styles.messages}>
+					<ForumIcon />
+					<div>35</div>
+				</div>
+			</div>
+		</div>
+	);
 };
