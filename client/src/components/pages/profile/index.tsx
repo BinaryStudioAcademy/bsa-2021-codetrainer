7384f7dbcb5b5fdb306a20afc8f04b9e179847f5
Vlad Zusko <vladzvu11@gmail.com>
2021-09-01 15:29:52 +0300

    Updated profile page

--- client/src/components/pages/profile/index.tsx
+++ client/src/components/pages/profile/index.tsx
@@ -7,10 +7,20 @@ import { IUser } from 'typings/common/IUser';
 interface IProfileProps {
 	error: string | null;
 	userInfo: IUser | null;
+	currentUser: IUser | null;
 	profileInfoProps: IProfileInfoProps;
+	followHandler: (id: string) => void;
+	unfollowHandler: (id: string) => void;
 }
 
-export const Profile = ({ userInfo, error, profileInfoProps }: IProfileProps) => {
+export const Profile = ({
+	userInfo,
+	error,
+	profileInfoProps,
+	currentUser,
+	followHandler,
+	unfollowHandler,
+}: IProfileProps) => {
 	return (
 		<div className={styles.profile}>
 			{error ? (
@@ -18,7 +28,12 @@ export const Profile = ({ userInfo, error, profileInfoProps }: IProfileProps) =>
 			) : (
 				userInfo && (
 					<>
-						<ProfileBio {...userInfo} />
+						<ProfileBio
+							user={userInfo}
+							currentUser={currentUser}
+							followHandler={followHandler}
+							unfollowHandler={unfollowHandler}
+						/>
 						<ProfileInfo {...profileInfoProps} />
 					</>
 				)
