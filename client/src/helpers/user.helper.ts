6550731beef080a4ac1bf988c039e12ba2fcd2a1
INTJT <74379701+INTJT@users.noreply.github.com>
2021-09-05 15:52:10 +0300

    fix user updating

--- client/src/helpers/user.helper.ts
+++ client/src/helpers/user.helper.ts
@@ -2,16 +2,14 @@ import { WebApi } from 'typings/webapi';
 import { getProfile } from 'services/github.service';
 import { IUser } from 'typings/common/IUser';
 
-export async function mapUserResponseToUser({ githubId, ...remains }: WebApi.Entities.IUser): Promise<IUser> {
+export async function mapUserResponseToUser(user: WebApi.Entities.IUser): Promise<IUser> {
 	try {
-		const githubProfile = githubId ? await getProfile(githubId) : undefined;
+		const githubProfile = user.githubId ? await getProfile(user.githubId) : undefined;
 		return {
-			...remains,
+			...user,
 			github: githubProfile,
 		};
 	} catch {
-		return {
-			...remains,
-		};
+		return user;
 	}
 }
