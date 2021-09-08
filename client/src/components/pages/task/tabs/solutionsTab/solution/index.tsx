e6e5df80567706d5f8a93a65b52d28dc6e8f027c
dammmula <eliza04102001@gmail.com>
2021-09-08 12:21:31 +0300

    Remove mocked comments

--- client/src/components/pages/task/tabs/solutionsTab/solution/index.tsx
+++ client/src/components/pages/task/tabs/solutionsTab/solution/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styles from './solution.module.scss';
 import PeopleIcon from '@material-ui/icons/People';
-import ForumIcon from '@material-ui/icons/Forum';
-import { useState } from 'react';
-import FeedMessage from 'components/pages/home/components/feed-message';
 import { WebApi } from 'typings/webapi';
 import { ROUTES } from 'constants/routes';
 import { Link } from 'react-router-dom';
@@ -13,18 +10,6 @@ export interface ISolutionProps {
 }
 
 export const Solution = ({ solution }: ISolutionProps) => {
-	const [isFeedOpened, setFeedOpened] = useState(false);
-	const user = {
-		id: 'fdsalfa',
-		// imageSource?: string;
-		name: 'name',
-		surname: 'surname',
-		clan: {
-			id: 'fdlsajfdsafdsagda',
-			name: 'fiksiki',
-		},
-	};
-
 	return (
 		<div className={styles.solution}>
 			<div className={styles.header}>
@@ -38,31 +23,6 @@ export const Solution = ({ solution }: ISolutionProps) => {
 			<div className={styles.code}>
 				<code>{solution.code}</code>
 			</div>
-			<div className={styles.footer}>
-				<div onClick={() => setFeedOpened(!isFeedOpened)} className={styles.messagesBtn}>
-					<ForumIcon />
-					<div>35</div>
-				</div>
-			</div>
-			{isFeedOpened && (
-				<div className={styles.messages}>
-					{[
-						'Awesome solution! ',
-						'So clean, readable and clever. Brilliant.',
-						"Oh wow, I didn't think to solve the null issue like this. Awesome solution, thanks for teaching me something new.",
-					].map((item) => {
-						return (
-							<FeedMessage
-								id={item}
-								key={item}
-								user={user}
-								body={item}
-								createdAt={String(solution.createdAt)}
-							/>
-						);
-					})}
-				</div>
-			)}
 		</div>
 	);
 };
